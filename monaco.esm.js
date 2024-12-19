7ab7bb9e976d8be77723da05feebce784dd4a34f
HardResetting <sribnij_d@web.de>
2024-12-19 15:55:44 +0100

    Fix print-view

--- monaco.esm.js
+++ monaco.esm.js
@@ -21,6 +21,17 @@ export class MonacoPlugin {
     this.monacoBaseUrl = this.options.monacoBaseUrl;
   }
 
+
+  initCurrentSlide = function () {
+    const currentSlide = this.deck.getCurrentSlide();
+    return this.initSlide(currentSlide);
+  }
+
+  destroyCurrentSlide = function () {
+    const currentSlide = this.deck.getCurrentSlide();
+    return this.destroySlide(currentSlide);
+  }
+
   async init() {
     await new Promise((resolve) =>
       this.loadScript(`${this.monacoBaseUrl}/min/vs/loader.js`, resolve)
@@ -30,93 +41,104 @@ export class MonacoPlugin {
     if (window.monaco) {
       this.log("[monaco] loaded");
       this.monaco = window.monaco;
+      window.revealMonaco = this;
     } else {
       throw new Error("Could not load Monaco");
     }
 
-    this.deck.on("slidechanged", (x) => this.onSlideChanged(x));
-
-    // see if there is an editor on the initial slide
-    this.deck.on("ready", _ => this.onSlideChanged());
+    this.deck.on("ready", _ => this.onReady());
   }
 
   loadScript(url, callback) {
-    let head = document.querySelector("head");
-    let script = document.createElement("script");
+    const head = document.querySelector("head");
+    const script = document.createElement("script");
     script.type = "text/javascript";
     script.src = url;
 
     // Wrapper for callback to make sure it only fires once
-    let finish = () => {
+    script.onload = () => {
       if (typeof callback === "function") {
         callback.call();
         callback = null;
       }
     };
 
-    script.onload = finish;
-
-    // IE
-    script.onreadystatechange = () => {
-      if (this.readyState === "loaded") {
-        finish();
-      }
-    };
-
-    // Normal browsers
+    // Append the script to the document
     head.appendChild(script);
   }
 
-  onSlideChanged(event) {
-    const state = {
-      previousSlide: event?.previousSlide || this.deck.getPreviousSlide(),
-      currentSlide: event?.currentSlide || this.deck.getCurrentSlide(),
-    };
-    this.log(state);
-
-    if (state.previousSlide) {
-      const codeBlocks = state.previousSlide.querySelectorAll(this.options.selector);
-      codeBlocks.forEach((codeBlock, index) => {
-        const editor = this.editors.get(codeBlock);
-        if (editor) {
-          const contents = editor.getModel().getValue().trimStart();
-          editor.dispose();
-          this.editors.delete(codeBlock);
-
-          const noscript = document.createElement("script");
-          noscript.setAttribute("type", "text/template");
-          noscript.innerHTML = contents;
-          codeBlock.appendChild(noscript);
-        }
-      });
+  onReady() {
+    if (this.deck.isPrintView()) {
+      this.initSlide(this.deck.getSlidesElement());
+      return;
     }
 
-    if (state.currentSlide) {
-      const codeBlocks = state.currentSlide.querySelectorAll(this.options.selector);
-      codeBlocks.forEach((codeBlock, index) => {
-        const scriptTemplateChild = codeBlock.querySelector("script[type='text/template']");
-        const initialCode = (scriptTemplateChild ? scriptTemplateChild.innerHTML : codeBlock.innerHTML).trimStart();
-        codeBlock.innerHTML = "";
-        const language = codeBlock.getAttribute("language") || codeBlock.getAttribute("data-language") || this.options.defaultLanguage;
-        monaco.editor.setTheme('vs-dark');
-        const editor = this.monaco.editor.create(codeBlock, {
-          ...this.options.editorOptions,
-          value: initialCode,
-          language: language
-        });
+    this.initCurrentSlide();
+    this.deck.on("slidechanged", _ => this.onSlideChanged());
+  }
 
-        editor.getModel().onDidChangeContent(e => {
-          const contentChangeEvent = new CustomEvent("reveal-monaco-content-change", { bubbles: true, detail: { textContent: editor.getModel().getValue() } });
-          codeBlock.dispatchEvent(contentChangeEvent);
-        });
+  onSlideChanged() {
+    const previousSlide = this.deck.getPreviousSlide(),
+      currentSlide = this.deck.getCurrentSlide();
+    this.log(previousSlide, currentSlide);
 
-        // Store the editor instance
-        this.editors.set(codeBlock, editor);
-
-        // Dispatch event for initial display of the editor
-        codeBlock.dispatchEvent(new CustomEvent("reveal-monaco-content-change", { bubbles: true, detail: { textContent: editor.getModel().getValue() } }));
-      });
+    if (previousSlide) {
+      this.destroySlide(previousSlide);
     }
+
+    if (currentSlide) {
+      this.initSlide(currentSlide);
+    }
+  }
+
+  initSlide(htmlElement) {
+    const codeBlocks = htmlElement.querySelectorAll(this.options.selector);
+    codeBlocks.forEach((codeBlock, index) => {
+      const potentialEditor = this.editors.get(codeBlock);
+      if (potentialEditor) {
+        console.warn("Tried initializing editor twice:", codeBlock)
+        return;
+      }
+
+      const scriptTemplateChild = codeBlock.querySelector("script[type='text/template']");
+      const initialCode = (scriptTemplateChild ? scriptTemplateChild.innerHTML : codeBlock.innerHTML).trimStart();
+      codeBlock.innerHTML = "";
+      const language = codeBlock.getAttribute("language") || codeBlock.getAttribute("data-language") || this.options.defaultLanguage;
+      monaco.editor.setTheme('vs-dark');
+      const editor = this.monaco.editor.create(codeBlock, {
+        ...this.options.editorOptions,
+        value: initialCode,
+        language: language
+      });
+
+      editor.getModel().onDidChangeContent(e => {
+        const contentChangeEvent = new CustomEvent("reveal-monaco-content-change", { bubbles: true, detail: { textContent: editor.getModel().getValue() } });
+        codeBlock.dispatchEvent(contentChangeEvent);
+      });
+
+      // Store the editor instance
+      this.editors.set(codeBlock, editor);
+
+      // Dispatch event for initial display of the editor
+      codeBlock.dispatchEvent(new CustomEvent("reveal-monaco-content-change", { bubbles: true, detail: { textContent: editor.getModel().getValue() } }));
+    });
+  }
+
+  destroySlide(htmlElement) {
+    const codeBlocks = htmlElement.querySelectorAll(this.options.selector);
+    codeBlocks.forEach((codeBlock, index) => {
+      const editor = this.editors.get(codeBlock);
+      if (editor) {
+        const contents = editor.getModel().getValue().trimStart();
+        editor.dispose();
+        this.editors.delete(codeBlock);
+
+        const noscript = document.createElement("script");
+        noscript.setAttribute("type", "text/template");
+        noscript.innerHTML = contents;
+        codeBlock.appendChild(noscript);
+      }
+    });
   }
 
   log(content) {
